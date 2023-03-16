265316816a1eb84fbad44bd4a7b5b3617089d70e
cworld1 <cworld0@qq.com>
2023-03-16 10:45:42 +0800

    feat(hooks): :zap: change the updateImage function

--- hooks/utils.py
+++ hooks/utils.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from main import changeTime
+
 # 用于展示视频（图像）
 import cv2
+
 
 def play_video():
     changeTime(25)
@@ -11,13 +12,13 @@ def pause():
     changeTime(200000)
 
 
-def updateImage(ui, img):
+def updateImage(label, img, width, height):
     # 通道转化
     RGBImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # 将图片转化成Qt可读格式
     image = QtGui.QImage(
         RGBImg, RGBImg.shape[1], RGBImg.shape[0], QtGui.QImage.Format_RGB888)
     # 加载图片,并自定义图片展示尺寸
-    image = QtGui.QPixmap(image).scaled(1200, 720)
+    image = QtGui.QPixmap(image).scaled(width, height)
     # 显示图片
-    ui.label.setPixmap(image)
+    label.setPixmap(image)
